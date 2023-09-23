f507a1dea9f1d3676174ac503b01005a85616c85
Bogdan-design <bogdanbw@gmail.com>
2023-09-23 22:30:29 +0200

    GET request

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,33 @@ import express, {Request,Response} from 'express';
 const app = express()
 const port = process.env.PORT || 3001
 
-app.get('/', (req: Request, res: Response) => {
-    const helloAnyone = 'Hello anyone';
-    res.send(helloAnyone)
+const products = [{title:'tomato'},{title: 'orange'}]
+const addresses = [{id:1,value:'Nezalejnasti 12'},{id:2,value: 'Selikaga 11'}]
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
 })
+
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+    const product = products.find(p=>p.title === req.params.productTitle)
+    if(product){
+        res.send(product)
+    }
+    res.send(404)
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    const address = addresses.find(a=>a.id === +req.params.id)
+    if(address){
+        res.send(address)
+    }
+    res.send(404)
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
