a8c630cdd3a0445db4dc1b1235eebe5bbfe16f13
Bogdan-design <bogdanbw@gmail.com>
2023-09-24 11:18:13 +0200

    delete request

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,45 @@
-import express, {Request,Response} from 'express';
+import express, {Request, Response} from 'express';
+
 const app = express()
 const port = process.env.PORT || 3001
 
-const products = [{title:'tomato'},{title: 'orange'}]
-const addresses = [{id:1,value:'Nezalejnasti 12'},{id:2,value: 'Selikaga 11'}]
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+const addresses = [{id: 1, value: 'Nezalejnasti 12'}, {id: 2, value: 'Selikaga 11'}]
 
 app.get('/products', (req: Request, res: Response) => {
-    res.send(products)
+    if (req.query.title) {
+        const searchString = req.query.title.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+    } else {
+        res.send(products)
+    }
 })
 
-app.get('/products/:productTitle', (req: Request, res: Response) => {
-    const product = products.find(p=>p.title === req.params.productTitle)
-    if(product){
+app.get('/products/:id', (req: Request, res: Response) => {
+    const product = products.find(p => p.id === +req.params.id)
+    if (product) {
+
         res.send(product)
     }
     res.send(404)
+})
+app.delete('/products/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id){
+            products.splice(i,1)
+            res.send(201)
+            return
+        }
+    }
+
 })
 
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
 })
-
 app.get('/addresses/:id', (req: Request, res: Response) => {
-    const address = addresses.find(a=>a.id === +req.params.id)
-    if(address){
+    const address = addresses.find(a => a.id === +req.params.id)
+    if (address) {
         res.send(address)
     }
     res.send(404)
