bb0ea39e917a3d208b727ee34ac3c08484d86d9c
Bogdan-design <bogdanbw@gmail.com>
2023-09-25 16:18:59 +0200

    DAL

--- src/routs/addresses-route.ts
+++ src/routs/addresses-route.ts
@@ -1,49 +1,16 @@
 import {Request, Response, Router} from "express";
 
-export const productsRouter = Router({})
+export const addressesRouter = Router({})
 
-const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
 
-productsRouter.get('/', (req: Request, res: Response) => {
-    if (req.query.title) {
-        const searchString = req.query.title.toString();
-        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
-    } else {
-        res.send(products)
-    }
+const addresses = [{id: 1, value: 'Nezalejnasti 12'}, {id: 2, value: 'Selikaga 11'}]
+addressesRouter.get('/', (req: Request, res: Response) => {
+    res.send(addresses)
 })
-productsRouter.get('/:id', (req: Request, res: Response) => {
-    const product = products.find(p => p.id === +req.params.id)
-    if (product) {
-
-        res.send(product)
-    }
-    res.send(404)
-})
-productsRouter.delete('/:id', (req: Request, res: Response) => {
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === +req.params.id) {
-            products.splice(i, 1)
-            res.send(204)
-            return
-        }
-    }
-    res.send(404)
-})
-productsRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = {
-        id: req.body.id,
-        title: req.body.title
-    }
-    products.push(newProduct)
-    res.status(201).send(newProduct)
-})
-productsRouter.put('/:id', (req: Request, res: Response) => {
-    const product = products.find(p => p.id === +req.params.id)
-    if (product) {
-        product.title = req.body.title
-        res.status(777)
-        res.send(product)
+addressesRouter.get('/:id', (req: Request, res: Response) => {
+    const address = addresses.find(a => a.id === +req.params.id)
+    if (address) {
+        res.send(address)
     }
     res.send(404)
 })
--- src/routs/products-route.ts
+++ src/routs/products-route.ts
@@ -1,16 +1,41 @@
 import {Request, Response, Router} from "express";
+import {productsRepository} from "../repositories/products-repository";
 
-export const addressesRouter = Router({})
+export const productsRouter = Router({})
 
-const addresses = [{id: 1, value: 'Nezalejnasti 12'}, {id: 2, value: 'Selikaga 11'}]
 
-addressesRouter.get('/', (req: Request, res: Response) => {
-    res.send(addresses)
+productsRouter.get('/', (req: Request, res: Response) => {
+    const foundProducts = productsRepository.findProducts(req.query.title?.toString())
+    res.send(foundProducts)
 })
-addressesRouter.get('/:id', (req: Request, res: Response) => {
-    const address = addresses.find(a => a.id === +req.params.id)
-    if (address) {
-        res.send(address)
+
+
+productsRouter.get('/:id', (req: Request, res: Response) => {
+    const product = productsRepository.getProductById(+req.params.id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
     }
-    res.send(404)
+})
+productsRouter.delete('/:id', (req: Request, res: Response) => {
+    const isDeleted = productsRepository.deleteProduct(+req.params.id)
+    if (isDeleted) {
+        res.send(204)
+    } else {
+        res.send(404)
+    }
+})
+productsRouter.post('/', (req: Request, res: Response) => {
+    const newProduct = productsRepository.createProduct(req.body.title)
+    res.status(201).send(newProduct)
+})
+productsRouter.put('/:id', (req: Request, res: Response) => {
+    const isUpDated = productsRepository.updateProduct(+req.params.id, req.body.title)
+    if (isUpDated) {
+        const product = productsRepository.getProductById(+req.params.id)
+        res.send(product)
+    } else {
+        res.send(404)
+    }
 })
